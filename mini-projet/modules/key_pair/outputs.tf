41b8980ee8bdcd2c5b5b6517f2d2f103ca6ce729
DonBikel <37923689+DonBikel@users.noreply.github.com>
2025-01-02 05:56:39 +0100

    push output keyname key_pair

--- mini-projet/modules/key_pair/outputs.tf
+++ mini-projet/modules/key_pair/outputs.tf
@@ -1,3 +1,8 @@
+output "key_name" {
+  description = "Nom de la paire de clés enregistrée dans AWS"
+  value       = aws_key_pair.this.key_name
+}
+
 output "private_key_path" {
   description = "Chemin du fichier de la clé privée localement stockée"
   value       = var.private_key_path
